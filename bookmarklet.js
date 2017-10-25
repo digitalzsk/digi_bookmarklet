953c0353e3b3f15ebe83a45ce392a2248fe36b93
aosm12193@gmail.com <aosm12193@gmail.com>
2017-10-25 14:32:44 +0200

    test

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,44 +1,55 @@
 (function() {
 	if(typeof jQuery == "undefined") {
-		alert('Error: jQuery nie je definované, napíšte prosím email na admin@digitalz.eu');
+		//alert('Error: jQuery nie je definované, napíšte prosím email na admin@digitalz.eu');
 		return;
 	}
 	else {
 		$ = jQuery;
-		$videos = $('iframe');
 
-		if($videos.length < 0 ) {
-			alert('Error: Na stránke sa nenachádzajú žiadne video iframe. Ak si myslíte, že je to chyba, napíšte prosím email na admin@digitalz.eu');
-			return;
-		}
-		else {
-			$.fn.populate = function(index) {
-				$('<textarea/>', {
-					id: 'digitalz_textarea_'+index,
-					style: "border: 2px solid #2e68e5;margin-bottom:20px;display:block;height:100px;width:"+$(this).attr('width')+";",
-					readonly: true,
-					onclick: "var box = jQuery('#digitalz_textarea_'+"+index+");box.select();document.execCommand('copy');" +
-					"box.text('Video bolo skopírované do vašej schránky.');box.css('font-size','30px');",
-					text: 'IFRAME TEXT TODO',
-					resize: 'none',
-				}).prependTo($(this).parent());
+		if (localStorage.getItem('digitalz_sme_video')) {
 
-				$('<p/>', {
-					id: 'digitalz_title_textarea_'+index,
-					style: 'margin:auto;text-align:center;display:block;font-size:30px;font-weight:bold;color:#2e68e5;',
-					text: 'Kód pre zdieľanie videa',
-				}).prependTo($(this).parent());
-			};
+			$videos = $('iframe');
+			$("[id^=digitalz_title_textarea_]").remove();
+			$("[id^=digitalz_textarea_]").remove();
 
-			var vimeo_count = 0;
-			$videos.each(function(index, element){
-				if($(element).attr('src').search('vimeo') > -1) {
-					vimeo_count += 1;
-					$(element).populate(index);
-				}
-			});
-			if(vimeo_count == 0) {
+
+			if ($videos.length < 0) {
 				alert('Error: Na stránke sa nenachádzajú žiadne video iframe. Ak si myslíte, že je to chyba, napíšte prosím email na admin@digitalz.eu');
+				return;
+			}
+			else {
+				$.fn.populate = function (index, style) {
+
+					var video_id = $(this).attr("src").substr(31, $(this).attr("src").search(/\?/) - 31);
+
+					$('<p/>', {
+						id: 'digitalz_title_textarea_' + index,
+						style: 'margin:auto;text-align:center;display:block;font-size:30px;font-weight:bold;color:#2e68e5;',
+						text: 'Kód pre zdieľanie videa',
+					}).insertBefore($(this).parent());
+
+					$('<textarea/>', {
+						id: 'digitalz_textarea_' + index,
+						style: style,
+						readonly: true,
+						onclick: "var box = jQuery('#digitalz_textarea_'+" + index + ");box.select();document.execCommand('copy');" +
+						"box.text('Video bolo skopírované do vašej schránky.');box.css('font-size','30px');box.attr('onclick','')",
+						text: '<iframe id="digitalz_vimeo_video_' + video_id + '" src="https://digisport.sk/wp-content/uploads/2017/09/digitalz_iframe.html?vimeo_video_id=' + video_id + '" ></iframe>',
+						resize: 'none',
+					}).insertBefore($(this).parent());
+				};
+
+				var vimeo_count = 0;
+				$videos.each(function (index, element) {
+					if ($(element).attr('src').search('vimeo') > -1) {
+						vimeo_count += 1;
+						var style = "border: 2px solid #2e68e5;margin-bottom:20px;display:block;height:100px;width:" + $(element).outerWidth() + "px;";
+						$(element).populate(index, style);
+					}
+				});
+				if (vimeo_count == 0) {
+					alert('Error: Na stránke sa nenachádzajú žiadne video iframe. Ak si myslíte, že je to chyba, napíšte prosím email na admin@digitalz.eu');
+				}
 			}
 		}
 	}
---
+++ external_script.js
@@ -0,0 +1,22 @@
+
+
+
+
+javascript: (function () {
+    var jsCode = document.createElement('script');
+    jsCode.setAttribute('src', 'https://digisport.sk/wp-content/uploads/2017/09/bookmarklet.js');
+    document.body.appendChild(jsCode);
+}());
+
+
+https://chriszarate.github.io/bookmarkleter/
+
+
+javascript:(function(){var%20t=document.createElement(%22script%22);t.setAttribute(%22src%22,%22http://digitalz.getforge.io/bookmarklet.js%22),document.body.appendChild(t)})();
+
+
+
+
+
+AKTUAL
+    javascript:(function(){var t=document.createElement("script");t.setAttribute("src","https://digisport.sk/wp-content/uploads/2017/09/bookmarklet.js?"+Date.now()),document.body.appendChild(t)})();
